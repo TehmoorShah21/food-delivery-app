f4706aefbfa95fcc599815fe57594feda2ef93fd
Abdul Wahab <tehmoor@trimulabs.com>
2022-11-21 14:15:09 +0500

    Hero section completed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,28 +1,12 @@
-// import Image from 'next/image'
 import Navbar from '../Navbar/Navbar'
-// import Hero from 'components/assests/Hero.png'
+
 import styles from './Header.module.sass'
 export default function Header() {
   return (
     <div className={styles.container}>
-      <Navbar />
-      {/* <div className={styles.img_container}>
-        <Image
-          src={Hero}
-          className={styles.img}
-          layout="fill"
-          alt={'Hero Background'}
-        />
+      <div>
+        <Navbar />
       </div>
-      <div className={styles.img_content}>
-        <p>Food app</p>
-        <h1>Why stay hungry when you can order from Bella Onojies</h1>
-        <p>Download the bella onojes food app now on</p>
-        <div>
-          <button className={styles.btn}>Playstore</button>
-          <button className={styles.btn}>App store</button>
-        </div>
-      </div> */}
     </div>
   )
 }
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,3 +1,21 @@
+// import hero_img from 'components/assests/hero_img.png'
+// import Image from 'next/image'
+import styles from './Hero.module.sass'
 export default function Hero() {
-  return <div>Hero</div>
+  return (
+    <div className={styles.hero}>
+      <div className={styles.hero_img}>
+        <div className={styles.hero_content}>
+          <div className={styles.hero_content_inner}>
+            <p>Food app</p>
+            <h1>Why stay hungry when you can order from Bella Onojies</h1>
+            <p>Download the bella onojes food app now on</p>
+
+            <button className={styles.btn}>Playstore</button>
+            <button className={styles.btn}>App store</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
